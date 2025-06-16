cc8cc2e7a3d4073a9dbce53ecbd8636c211d25ed
mellowmorgan <morgan.waites@gmail.com>
2025-06-16 15:13:49 -0700

    WIP: resolve importmaps and turbo issues

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 
-import "@hotwired/turbo-rails"
-import "controllers";
-import { Turbo } from "@hotwired/turbo-rails";
-Turbo.config.forms.mode = "on";
+import { Application } from "@hotwired/stimulus"
 
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+export { application }
