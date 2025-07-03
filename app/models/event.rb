2f976827ed78a41f827d336cb40fca1c636e1e97
mellowmorgan <morgan.waites@gmail.com>
2025-07-03 13:38:32 -0700

    Fix: make sure needs_updating method checks that events are passed

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,12 +4,16 @@ class Event < ApplicationRecord
   has_many :earnings, dependent: :destroy
   validates :title, presence: true
   validates :start_time, presence: true
-  validates :is_work, presence: true
   validates :user, presence: true
   scope :needs_updating, -> {where("dismiss IS NOT TRUE AND ( end_time IS NULL AND start_time < ? OR (end_time < ?))", DateTime.now, DateTime.now)}
 
   def needs_updating?
-    ((self.earnings.none? || self.expenses.none?) && self.is_work && dismiss==false) ||
-    (self.earnings.none? && self.expenses.none? && !self.is_work && dismiss==false)
+    
+      ((self.earnings.none? || self.expenses.none?) && self.is_work && dismiss==false && self.passed?) ||
+      (self.earnings.none? && self.expenses.none? && !self.is_work && dismiss==false && self.passed?)
+  end
+
+  def passed?
+    end_time && end_time < DateTime.now || end_time.nil? && start_time < DateTime.now
   end
 end
